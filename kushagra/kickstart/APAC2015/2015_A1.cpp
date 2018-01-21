8ee66f367b7ab1b7ff314988a6b523519d2122ed
kushagraaa <kushagra.iitkgp@gmail.com>
2018-01-21 14:33:24 +0530

    APAC2016

---
+++ kushagra/kickstart/APAC2015/2015_A1.cpp
@@ -0,0 +1,54 @@
+/*
+	Author		:	Kushagra Aggarwal
+	Institute	:	IIT Kharagpur
+*/
+
+#include <bits/stdc++.h>
+
+#define max3(a,b,c) (max(max(a,b),c))
+#define min3(a,b,c) (min(min(a,b),c))
+#define inf INT_MAX
+#define mod 1000000007
+
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+
+#define all(c) (c).begin(),(c).end()
+#define allr(c) (c).rbegin(),(c).rend()
+#define asort(c) sort(all(c))
+#define dsort(c) sort(allr(c))
+
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+using namespace std;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+
+bool rec(ll k, ll n){
+	if(k==(n>>1))
+		return 0;
+	
+	if(k<(n>>1))
+		return rec(k,(n>>1));
+
+	return !rec(n-k,(n>>1));
+}
+
+int main(){
+	int t;
+	cin >> t;
+	int cnt =1;
+	while(t--){
+		ll k;
+		cin >> k;
+		// k--;
+		printf("Case #%d: %d\n", cnt++ , rec(k,((ll)1)<<62));
+		// cout << "Case #" << cnt++ <<": " << rec(k,((ll)1)<<62)<< endl;
+	}
+}
