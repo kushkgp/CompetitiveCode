5b2d1c274ccceae358c2b66b1d2c7b35be361af3
kushagraaa <gulabarora07@gmail.com>
2018-03-01 01:50:40 +0530

    sieve

---
+++ gulab/spoj/number-theory/AMR11E.cpp
@@ -0,0 +1,60 @@
+/*
+	Author		:	Gulab Arora
+	Institute	:	IIT Kharagpur
+*/
+
+#include <bits/stdc++.h>
+
+#define max3(a,b,c) (max(max(a,b),c))
+#define min3(a,b,c) (min(min(a,b),c))
+#define inf 1e9
+
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+
+#define all(c) (c).begin(),(c).end()
+#define allr(c) (c).rbegin(),(c).rend()
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define F1(x,y,z) for(int x = y; x <= z; x++)
+#define F2(x,y,z) for(int x = y; x >= z; x--)
+#define MOD 1000000007
+
+using namespace std;
+typedef long long int ll;
+template <typename X> using v = vector<X>;
+template <typename X> using vv = v<v<X> >;
+typedef v<int> vi;
+typedef v<vi> vvi;
+typedef pair<int,int> pii;
+typedef v<pii> vpii;
+
+#define N 10000
+int phi[10001];
+vi comp;
+
+void precompute(){
+	memset(phi,0,sizeof(phi));
+	F1(i,2,N){
+		if(!phi[i])
+			for(int j = 2; (ll)j*i<=N; j++)
+				phi[i*j]++;
+	}
+	F1(i,2,N)
+		if(phi[i]>2)
+			comp.pb(i);
+}
+
+int main(){
+	BOOST;
+	precompute();
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		cout<<comp[n-1]<<endl;
+	}
+}
