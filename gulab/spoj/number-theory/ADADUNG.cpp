0cc14dc93658e285b0f7f411667954a8acfb0f9b
kushagraaa <gulabarora07@gmail.com>
2018-03-01 02:11:09 +0530

    one more

---
+++ gulab/spoj/number-theory/ADADUNG.cpp
@@ -0,0 +1,54 @@
+/*
+	Author		:	Gulab Arora
+	Institute	:	IIT Kharagpur
+*/
+
+#include <bits/stdc++.h>
+
+#define max3(a,b,c) (max(max(a,b),c))
+#define min3(a,b,c) (min(min(a,b),c))
+#define inf 1e9
+
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+
+#define all(c) (c).begin(),(c).end()
+#define allr(c) (c).rbegin(),(c).rend()
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define F1(x,y,z) for(int x = y; x <= z; x++)
+#define F2(x,y,z) for(int x = y; x >= z; x--)
+#define MOD 1000000007
+
+using namespace std;
+typedef long long int ll;
+template <typename X> using v = vector<X>;
+template <typename X> using vv = v<v<X> >;
+typedef v<int> vi;
+typedef v<vi> vvi;
+typedef pair<int,int> pii;
+typedef v<pii> vpii;
+
+#define N 10000000
+int comp[10000001];
+
+void precompute(){
+	ll x = 1;
+	comp[1] = 0;
+	F1(i,2,N)
+		comp[i] = ((ll)i*comp[i-1]+((i&1)?-1:1))%MOD;
+}
+
+int main(){
+	BOOST;
+	precompute();
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		cout<<comp[n]<<endl;
+	}
+}
