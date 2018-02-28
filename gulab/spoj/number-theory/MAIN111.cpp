959bdb90ed66bad1d7c2ff47031d8e0361af3c10
kushagraaa <gulabarora07@gmail.com>
2018-03-01 01:48:19 +0530

    changed main111

--- gulab/spoj/number-theory/MAIN111.cpp
+++ gulab/spoj/number-theory/MAIN111.cpp
@@ -14,6 +14,7 @@
 #define pb push_back
 #define mp make_pair
 
+
 #define all(c) (c).begin(),(c).end()
 #define allr(c) (c).rbegin(),(c).rend()
 #define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
