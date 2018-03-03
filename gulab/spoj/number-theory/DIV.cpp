3944fcb3f38916fdcfc130a61d795df0f0baf4c4
kushagraaa <gulabarora07@gmail.com>
2018-03-03 13:06:27 +0530

    some more questions

--- gulab/spoj/number-theory/DIV.cpp
+++ gulab/spoj/number-theory/DIV.cpp
@@ -29,39 +29,43 @@ template <typename X> using vv = v<v<X> >;
 typedef v<int> vi;
 typedef v<vi> vvi;
 typedef pair<int,int> pii;
-typedef vector<pii> vpii;
-int a,b,ans,x;
+typedef v<pii> vpii;
+
+#define N 1000000
+int phi[1000001];
 
 void precompute(){
-	int n = sqrt(b)+1;
-	vi phi(n+1,0);
-	vi primes;
-	for(int i = 2; (ll)i*i<=b; i++){
+	memset(phi,0,sizeof(phi));
+	F1(i,2,N){
 		if(!phi[i]){
-			primes.pb(i);
-			for(int j = i; (ll)j*i<=n; j++){
-				phi[j*i] = 1;
-			}
+			phi[i] = i;
+			for(int j = i; (ll)j*i<=N; j++)
+				phi[i*j] = i;
 		}
 	}
-	F1(i,0,(b-a)){
-		int val = 1;
-		int y = i+a;
-		F1(j,0,(int)primes.size()-1){
-			int cnt = 1;
-			while(!(y%primes[j]))
-				cnt++,y/=primes[j];
-			val*=cnt;
+
+	int prevsum = 0, curr = 0;
+	F1(i,2,N){
+		int cnt = 0, t = i;
+		while(!(t%phi[i]))
+			cnt++, t/=phi[i];
+		if(t>1){
+			int cnt2 = 0, t2 = t;
+			while(!(t2%phi[t]))
+				cnt2++, t2/=phi[t];
+			curr = (t2==1)&&(phi[cnt+1]==cnt+1)&&(phi[cnt2+1]==cnt2+1)&&(cnt!=cnt2);
 		}
-		if(y!=1)
-			val*=2;
-		if(val == x)
-			ans++;
+		else{
+			int temp = cnt+1;
+			curr = (phi[temp/phi[temp]]==temp/phi[temp])&&(phi[temp]!=phi[temp/phi[temp]]);
+		}
+		prevsum += curr;
+		if(curr&&!(prevsum%9))
+			cout<<i<<endl;
 	}
 }
 
 int main(){
-	cin>>a>>b>>x;
+	BOOST;
 	precompute();
-	cout<<ans<<endl;
 }
